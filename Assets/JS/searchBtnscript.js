186ca4f5a6d0083a85bde85b10da1ec1c299d695
Chris <kjman26@gmail.com>
2022-09-29 12:05:44 -0700

    beginning of search fetch

--- Assets/JS/searchBtnscript.js
+++ Assets/JS/searchBtnscript.js
@@ -1,7 +1,20 @@
-const spoonApiKey = "6bd62a9ff28a42a08d35b2fccec9fd9e"
-const userInput = 
-const spoonSearchType = `https://api.spoonacular.com/recipes/complexSearch?query=${name}`;
-//need to add const name = userInput
-const spoonSearchIngredient = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${userIngredient}`
+// const spoonApiKey = "6bd62a9ff28a42a08d35b2fccec9fd9e"
+const userName = $("#sBtn").val();
 
-fetch()
+console.log(userName)
+const spoonSearchType = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${spoonApiKey}query=${userName}`;
+//need to add const name = userInput
+// const spoonSearchIngredient = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${userIngredient}`
+
+function spoonSearch(){
+fetch(spoonSearchType)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+    })
+    };
+
+
+$("#searchBtn").on("click", spoonSearch);
